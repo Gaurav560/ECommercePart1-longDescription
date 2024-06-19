19b84f1977933486402d46e9be994c5c382d8642
GAURAV SHARMA <gsharma1154@gmail.com>
2024-06-19 14:52:01 +0530

    part 2 long description

---
+++ src/main/java/com/telusko/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.telusko.service;
+
+import com.telusko.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    //  Product createProduct(Product product) throws Exception;
+    List<Product> getAllProducts();
+
+    Product getProductById(int id);
+}
